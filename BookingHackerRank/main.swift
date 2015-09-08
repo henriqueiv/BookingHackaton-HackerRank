efab22b1d31324540c1e485994621398416d4ce7
Henrique Valcanaia <henrique.valcanaia@hotmail.com>
2015-09-08 19:07:58 -0300

    Initial Commit

---
+++ BookingHackerRank/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BookingHackerRank
+//
+//  Created by Henrique Valcanaia on 9/8/15.
+//  Copyright (c) 2015 DarkShine. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
